a2cd705822a5b77c61cc3514e30c3f10673d51b5
Saidaamrahova <126199748+Saidaamrahova@users.noreply.github.com>
2023-05-24 23:50:33 +0400

    section2

--- js/myscripts.js
+++ js/myscripts.js
@@ -4,3 +4,26 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
     },
 });
+
+$(".owl-carousel").owlCarousel({
+    responsiveClass: true,
+    nav: false,
+    items: 4,
+    loop: true,
+    dots: true,
+    autoWidth: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 4,
+        },
+    },
+});
+
